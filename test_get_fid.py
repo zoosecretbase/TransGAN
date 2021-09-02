c4e157daeef182b154378c46d4183c0b71f7bdfd
Tori.Zoo <zoosecretbase.chicken@gmail.com>
2021-09-03 08:24:13 +0900

    GPUある環境で動かすためのテスト #3

---
+++ test_get_fid.py
@@ -0,0 +1,18 @@
+from test_fid_score import *
+from test_real_imgs import *
+
+
+airplane = 0
+automobile = 0
+bird = 1
+cat = 0
+deer = 0
+dog = 0
+frog = 1
+horse = 0
+ship = 0
+truck = 0
+
+real_imgs = test_real_imgs_row_data(airplane, automobile, bird, cat, deer, dog, frog, horse, ship, truck)
+
+
